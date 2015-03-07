2f123f443324b886bf6d4f6dcd0b6f3b00ab14a1
Quy Nguyen <quy.nguyen@gmail.com>
2015-03-07 11:42:41 -0800

    ...

--- greeters.ts
+++ greeters.ts
@@ -1,8 +1,19 @@
-function greeter(person: string) {
-	return "Hello " + person;
+class Student {
+	fullname: string;
+	constructor(public firstname, public middleinitial, public lastname) {
+		this.fullname = firstname + " " + middleinitial + lastname;
+	}
 }
 
-var user = "Jane User";
+interface Person {
+	firstname: string
+	lastname: string
+}
 
+function greeter(person: Person) {
+	return "Hello " + person.firstname + " " + person.lastname;
+}
+
+var user = new Student("Jane", "M", "User");
 
 document.body.innerHTML = greeter(user);
